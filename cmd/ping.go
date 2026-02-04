72e45cc74e01e1b94f0ab10e60a41601fdcd753b
Arthur Oliveira <arthurr.ufcg@gmail.com>
2026-02-04 18:14:40 -0300

    chore: rename project from gdch-cli to statectl

--- cmd/ping.go
+++ cmd/ping.go
@@ -5,7 +5,7 @@ package cmd
 
 import (
 	"context"
-	"gdch-cli/internal/ping"
+	"statectl/internal/ping"
 	"time"
 
 	"github.com/spf13/cobra"
--- cmd/version.go
+++ cmd/version.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"gdch-cli/internal/version"
+	"statectl/internal/version"
 
 	"github.com/spf13/cobra"
 )
