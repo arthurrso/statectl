903e1485183a5944347662255b6bfafc08f281dc
Arthur Oliveira <arthurr.ufcg@gmail.com>
2026-02-05 22:21:55 -0300

    changed go version to avoid conflicts with bazel and kubernetes, created api structs

---
+++ internal/api/statectl/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains the experimental API types for statectl.
+//
+// This API is personal, experimental, and may change at any time.
+package v1alpha1
---
+++ internal/api/statectl/v1alpha1/instance_types.go
@@ -0,0 +1,28 @@
+package v1alpha1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+type Instance struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   InstanceSpec   `json:"spec,omitempty"`
+	Status InstanceStatus `json:"status,omitempty"`
+}
+
+type InstanceSpec struct {
+	DesiredState string `json:"desiredState"`
+}
+
+type InstanceStatus struct {
+	CurrentState string `json:"currentState"`
+}
+
+// +kubebuilder:object:root=true
+type InstanceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Instance `json:"items"`
+}
